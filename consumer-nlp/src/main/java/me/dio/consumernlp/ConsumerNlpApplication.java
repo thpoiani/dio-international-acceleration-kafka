72640b8f58cb6ad85287a44acafb73df188d4f71
Thiago Poiani <thpoiani@gmail.com>
2023-10-20 22:38:03 -0600

    analyze sentiment

--- consumer-nlp/src/main/java/me/dio/consumernlp/ConsumerNlpApplication.java
+++ consumer-nlp/src/main/java/me/dio/consumernlp/ConsumerNlpApplication.java
@@ -1,13 +1,37 @@
 package me.dio.consumernlp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ConsumerNlpApplication {
+    Logger logger = LoggerFactory.getLogger(ConsumerNlpApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerNlpApplication.class, args);
-	}
+    @Autowired
+    private SentimentAnalysisService sentimentAnalysisService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerNlpApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            Stream.of("I just lifted weights and now my arms feel floppy",
+                      "good morning!!! its 12:25AM at California.",
+                      "hates how just being friends isnt good enough for some",
+                      "Great game, West Brom pushed us all the way. Al least we wont go down now with 41 points")
+                  .map(sentimentAnalysisService::analyzeSentiment)
+                  .forEach(s -> logger.info("Sentiment: {}", s));
+        };
+    }
 
 }
---
+++ consumer-nlp/src/main/java/me/dio/consumernlp/SentimentAnalysisService.java
@@ -0,0 +1,12 @@
+package me.dio.consumernlp;
+
+import edu.stanford.nlp.simple.Sentence;
+import edu.stanford.nlp.simple.SentimentClass;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SentimentAnalysisService {
+    public SentimentClass analyzeSentiment(String message) {
+        return new Sentence(message).sentiment();
+    }
+}
