8932199a40305af7f8667f9a390a71497e34f6f9
Thiago Poiani <thpoiani@gmail.com>
2023-10-20 21:39:18 -0600

    start.spring.io - consumer-nlp

---
+++ consumer-nlp/src/main/java/me/dio/consumernlp/ConsumerNlpApplication.java
@@ -0,0 +1,13 @@
+package me.dio.consumernlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerNlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerNlpApplication.class, args);
+	}
+
+}
