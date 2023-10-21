ad7c50ff38e53acf7f4099ce3ed1fa36ac5ca2a2
Thiago Poiani <thpoiani@gmail.com>
2023-10-20 21:39:24 -0600

    start.spring.io - producer-twitter

---
+++ producer-twitter/src/main/java/me/dio/producertwitter/ProducerTwitterApplication.java
@@ -0,0 +1,13 @@
+package me.dio.producertwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerTwitterApplication.class, args);
+	}
+
+}
---
+++ producer-twitter/src/test/java/me/dio/producertwitter/ProducerTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.producertwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerTwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
